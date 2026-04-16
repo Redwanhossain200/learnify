cee0e5df30c1a235c744823a130dde1b43fb81b4
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-04-16 15:00:56 +0600

    code organized

--- JS/first.js
+++ JS/first.js
@@ -1,47 +1,47 @@
-console.log("Name");
+console.log('Name');
 
-console.log("Hello Guys");
+console.log('Hello Guys');
 
-console.log("Welcome to JavaScript");
+console.log('Welcome to JavaScript');
 
-console.log("This is my first JavaScript code");
+console.log('This is my first JavaScript code');
 
-console.log("I am learning JavaScript");
+console.log('I am learning JavaScript');
 
-console.log("I love coding in JavaScript");
+console.log('I love coding in JavaScript');
 
-console.log("JavaScript is fun!");
+console.log('JavaScript is fun!');
 
 console.log("Let's build something amazing");
 
-console.log("Happy coding!");
+console.log('Happy coding!');
 
-console.log("Goodbye!");
+console.log('Goodbye!');
 
-console.log("See you next time!");
+console.log('See you next time!');
 
-console.log("Stay safe!");
+console.log('Stay safe!');
 
-console.log("Keep practicing!");
+console.log('Keep practicing!');
 
-console.log("You can do it!");
+console.log('You can do it!');
 
-console.log("Believe in yourself!");
+console.log('Believe in yourself!');
 
-console.log("Never give up!");
+console.log('Never give up!');
 
-console.log("Success is near!");
+console.log('Success is near!');
 
-console.log("Keep pushing forward!");
+console.log('Keep pushing forward!');
 
-console.log("Dream big!");
+console.log('Dream big!');
 
-console.log("Make it happen!");
+console.log('Make it happen!');
 
-console.log("Enjoy the journey!");
+console.log('Enjoy the journey!');
 
-console.log("Stay positive!");
+console.log('Stay positive!');
 
-console.log("Embrace challenges!");
+console.log('Embrace challenges!');
 
-console.log("Learn from mistakes!");
+console.log('Learn from mistakes!');
